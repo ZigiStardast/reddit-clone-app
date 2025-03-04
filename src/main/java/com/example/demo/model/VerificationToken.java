b7fe40acd272552565ebcdfeb8860c447a7c9735
ZigiStardast <129152646+ZigiStardast@users.noreply.github.com>
2025-03-04 18:30:42 +0100

    dodati svi modeli i repo, sredjeni properties

--- src/main/java/com/example/demo/model/VerificationToken.java
+++ src/main/java/com/example/demo/model/VerificationToken.java
@@ -1,4 +1,23 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "token")
 public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+    private Instant expiryDate;
 }
---
+++ src/main/java/com/example/demo/model/VoteType.java
@@ -0,0 +1,8 @@
+package com.example.demo.model;
+
+public enum VoteType {
+    UPWOTE(1), DOWNWOTE(-1);
+
+    VoteType(int direction) {
+    }
+}
