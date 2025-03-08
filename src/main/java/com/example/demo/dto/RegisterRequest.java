d81d4b023b11a1417e7f7a5649b4811a97039113
ZigiStardast <129152646+ZigiStardast@users.noreply.github.com>
2025-03-08 20:58:45 +0100

    user registration, testirati posle

---
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto;
+
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String email;
+}
---
+++ src/main/java/com/example/demo/service/MailService.java
@@ -0,0 +1,36 @@
+package com.example.demo.service;
+
+import com.example.demo.exceptions.SpringRedditException;
+import com.example.demo.model.NotificationEmail;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class MailService {
+
+    private final JavaMailSender mailSender;
+    private final MailContentBuilder mailContentBuilder;
+
+    void sendMail(NotificationEmail notificationEmail) {
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("springreddit@email.com");
+            messageHelper.setSubject(notificationEmail.getSubject());
+            messageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
+        };
+
+        try {
+            mailSender.send(messagePreparator);
+            log.info("Email sent successfully");
+        } catch (MailException e) {
+            throw new SpringRedditException("Exception occured when sending mail to " + notificationEmail.getRecipient());
+        }
+    }
+}
