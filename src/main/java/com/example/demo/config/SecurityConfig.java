e4a283416ef1530c4c7c1036fd8fefad3861d7c2
ZigiStardast <129152646+ZigiStardast@users.noreply.github.com>
2025-03-10 21:34:36 +0100

    uradjena verifikacija korisnika

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -15,6 +15,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -1,22 +1,27 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.RegisterRequest;
+import com.example.demo.exceptions.SpringRedditException;
 import com.example.demo.model.NotificationEmail;
 import com.example.demo.model.User;
 import com.example.demo.model.VerificationToken;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.repository.VerificationTokenRepository;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import javax.swing.text.html.Option;
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class AuthService {
 
     private final PasswordEncoder passwordEncoder;
@@ -36,13 +41,18 @@ public class AuthService {
         user.setCreatedAt(Instant.now());
         user.setVerified(false);
 
-        userRepository.save(user);
+        try {
+            userRepository.save(user);
+            String token = generateVerificationToken(user);
 
-        String token = generateVerificationToken(user);
-        mailService.sendMail(new NotificationEmail("Please Activate Your Account", user.getUsername(),
-                "Thank you for signing up for Spring Reddit. Please click on the below "+
-                        "url to activate your account: " +
-                        "http://localhost:8080/api/auth/accountVerification/" + token));
+            mailService.sendMail(new NotificationEmail("Please Activate Your Account", user.getEmail(),
+                    "Thank you for signing up for Spring Reddit. Please click on the below "+
+                            "url to activate your account: " +
+                            "http://localhost:8080/api/auth/accountVerification/" + token));
+        } catch (Exception e) {
+            log.error("Error occurred during signup", e);
+            throw new RuntimeException("Registration failed");
+        }
 
     }
 
@@ -55,5 +65,21 @@ public class AuthService {
         verificationTokenRepository.save(verificationToken);
 
         return token;
+    }
+
+    @Transactional
+    public void verifyAccount(String token) {
+        Optional<VerificationToken> verificationToken = verificationTokenRepository.findByToken(token);
+        verificationToken.orElseThrow(() -> new SpringRedditException("Invalid verification token"));
+
+        fetchUserAndVerify(verificationToken.get());
+    }
+
+    private void fetchUserAndVerify(VerificationToken verificationToken) {
+        String username = verificationToken.getUser().getUsername();
+        Optional<User> user = userRepository.findByUsername(username);
+        user.orElseThrow(() -> new SpringRedditException("User not found with name " + username));
+        user.get().setVerified(true);
+        userRepository.save(user.get());
     }
 }
